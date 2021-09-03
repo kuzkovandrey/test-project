a0ae0312536a451d062eb753b19b9e70593c678e
kuzkovandrey <kuzkovandrey@gmail.com>
2021-09-03 22:43:32 +0300

    Added about-user features, auth service, user model

---
+++ src/app/about-user/about-user.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {AboutUserComponent} from "./about-user.component";
+import {AboutUserRoutingModule} from "./about-user-routing.module";
+
+@NgModule({
+  declarations: [
+    AboutUserComponent
+  ],
+  imports: [
+    AboutUserRoutingModule
+  ]
+})
+export class AboutUserModule {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from "@angular/router";
 
 const routes: Routes = [
   {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'about-user', loadChildren: () => import('./about-user/about-user.module').then(m => m.AboutUserModule)},
   {path: '', redirectTo: 'auth', pathMatch: 'full'}
 ]
 
